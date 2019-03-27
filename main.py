60176e3ec94cb2dcbbdd911589b9938d8177e5af
cechus <cechuscsc4@gmail.com>
2019-03-27 19:51:56 -0400

    enhancements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+# import requests
+from flask import Flask, request, make_response, redirect, render_template, session, json
 # from flask_bootstrap import Bootstrap
 # from flask_wtf import FlaskForm
 
@@ -10,6 +11,45 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar Cafe', 'Enviar Solicitud de compra', 'ENtregar video al']
 
+datas = [
+
+        {
+            "userId": 7,
+            "id": 65,
+            "title": "consequatur id enim sunt et et",
+            "body": "voluptatibus ex esse s"
+        },
+        {
+            "userId": 7,
+            "id": 66,
+            "title": "repudiandae ea animi iusto",
+            "body": "officia veritatis teneto saepe"
+        },
+        {
+            "userId": 7,
+            "id": 67,
+            "title": "aliquid eos sed fuga est maxime repellendus",
+            "body": "reprehenderit id nostressitatibus molestiae"
+        },
+        {
+            "userId": 7,
+            "id": 68,
+            "title": "odio quis facere architecto reiciendis optio",
+            "body": "magnam molestiae perfeorro velit"
+        },
+        {
+            "userId": 7,
+            "id": 69,
+            "title": "fugiat quod pariatur odit minima",
+            "body": "officiisudiandae asperiores et saepe a"
+        },
+        {
+            "userId": 7,
+            "id": 70,
+            "title": "voluptatem laborum magni",
+            "body": "sunt repdolore"
+        }
+    ]
 
 @app.errorhandler(404)
 def not_found(error):
@@ -41,3 +81,71 @@ def hello():
 
     }
     return render_template('hello.html', **context)
+
+
+@app.route('/api/v1/get-prices')
+def getprices():
+    # return requests.get('https://jsonplaceholder.typicode.com/posts').content
+    data = [
+
+        {
+            "userId": 7,
+            "id": 65,
+            "title": "consequatur id enim sunt et et",
+            "body": "voluptatibus ex esse s"
+        },
+        {
+            "userId": 7,
+            "id": 66,
+            "title": "repudiandae ea animi iusto",
+            "body": "officia veritatis teneto saepe"
+        },
+        {
+            "userId": 7,
+            "id": 67,
+            "title": "aliquid eos sed fuga est maxime repellendus",
+            "body": "reprehenderit id nostressitatibus molestiae"
+        },
+        {
+            "userId": 7,
+            "id": 68,
+            "title": "odio quis facere architecto reiciendis optio",
+            "body": "magnam molestiae perfeorro velit"
+        },
+        {
+            "userId": 7,
+            "id": 69,
+            "title": "fugiat quod pariatur odit minima",
+            "body": "officiisudiandae asperiores et saepe a"
+        },
+        {
+            "userId": 7,
+            "id": 70,
+            "title": "voluptatem laborum magni",
+            "body": "sunt repdolore"
+        }
+    ]
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/api/v1/price', methods=['POST'])
+def show_post():
+    req = request.get_json()
+    # return req
+    res = ['default']
+    for x in datas:
+        if x['id'] == int(req['id']):
+            res = x
+            break
+    response = app.response_class(
+        response=json.dumps(res),
+        # response=json.dumps(req),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
